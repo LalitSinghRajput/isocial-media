3eda7cfb55dbce39746c04b887d0bf5d29cd2e1d
Lalit <lalitsr24@gmail.com>
2022-12-18 13:23:32 +0530

    changes made for deployment

---
+++ client/build/precache-manifest.f468bcbf69612b8bff4e4947123055ab.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a11b872669a85b834a421659d330e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "725a1079e3726275199b",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "7f17b847ccaf49a558f3",
+    "url": "/static/css/main.67be8ab8.chunk.css"
+  },
+  {
+    "revision": "725a1079e3726275199b",
+    "url": "/static/js/2.21147d51.chunk.js"
+  },
+  {
+    "revision": "10206f115af8fcc6b02d5c2a15f446d6",
+    "url": "/static/js/2.21147d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f17b847ccaf49a558f3",
+    "url": "/static/js/main.c5b1674c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "aba9de2d23eff684aab3d2d4bbde5f30",
+    "url": "/static/media/addIcon.aba9de2d.png"
+  },
+  {
+    "revision": "2db131805241a6a51e1ffd1cad3e1e50",
+    "url": "/static/media/deleteIcon.2db13180.png"
+  },
+  {
+    "revision": "43149f746f16a26249925a0ab7dc2193",
+    "url": "/static/media/done.43149f74.png"
+  },
+  {
+    "revision": "2068186efa2e589b19349819e8eee59d",
+    "url": "/static/media/homepage.2068186e.png"
+  },
+  {
+    "revision": "ec730d1c20f41455639550362348c1fd",
+    "url": "/static/media/iSocialMediaLogo.ec730d1c.png"
+  },
+  {
+    "revision": "ad80c8ddf7ba2c02180c3e3732a5ce31",
+    "url": "/static/media/loader.ad80c8dd.gif"
+  },
+  {
+    "revision": "a1155837ef3e36440eb687c3399e2abc",
+    "url": "/static/media/loginUser.a1155837.png"
+  },
+  {
+    "revision": "58e6e94cdc417c4c670a87e5a08a89d5",
+    "url": "/static/media/logoutIcon.58e6e94c.png"
+  },
+  {
+    "revision": "eb185b9d9cc717b1e394a8289eadbb33",
+    "url": "/static/media/media.eb185b9d.png"
+  },
+  {
+    "revision": "46b54015aea37ecec604c65025af2201",
+    "url": "/static/media/updateAccount.46b54015.png"
+  },
+  {
+    "revision": "e9ace2e2dac30ed544ae393f52a0a0e0",
+    "url": "/static/media/user.e9ace2e2.png"
+  }
+]);
--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://busy-red-goat-boot.cyclic.app/api/",
-    // baseURL: "http://localhost:8000/api/",
+    baseURL: "http://localhost:8000/api/",
 })
